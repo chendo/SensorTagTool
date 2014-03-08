f7ea62d74dcdfa0beee80125cc552ce22f23579b
Jack Chen (chendo) <github@chen.do>
2014-03-08 20:16:56 +1100

    Initial Commit

---
+++ SensorTagTool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SensorTagTool
+//
+//  Created by Jack Chen on 8/03/2014.
+//  Copyright (c) 2014 chendo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
